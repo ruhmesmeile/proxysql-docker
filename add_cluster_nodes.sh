60f982d5d0148747a1baac3007deb7d28ae9ab73
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2016-12-12 14:23:41 +0100

    Use secure etcd2 connection

--- add_cluster_nodes.sh
+++ add_cluster_nodes.sh
@@ -2,7 +2,7 @@
 
 ipaddr=$(hostname -i | awk ' { print $1 } ')
 
-for i in $(curl http://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/$CLUSTER_NAME/ | jq -r '.node.nodes[]?.key' | awk -F'/' '{print $(NF)}')
+for i in $(curl --key /etc/docker/ssl/calculonc-key.pem --cert /etc/docker/ssl/calculonc.pem --cacert /etc/docker/ssl/ca.pem https://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/$CLUSTER_NAME/ | jq -r '.node.nodes[]?.key' | awk -F'/' '{print $(NF)}')
 do
 	echo $i 
         mysql -h $i -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL ON *.* TO '$MYSQL_PROXY_USER'@'$ipaddr' IDENTIFIED BY '$MYSQL_PROXY_PASSWORD'"
