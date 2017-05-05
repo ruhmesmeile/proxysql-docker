a7789359f83303796252692f9bc4676d83bd8a97
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2017-05-05 15:53:58 +0200

    Don't add credentials to openly visible repos

--- add_cluster_nodes.sh
+++ add_cluster_nodes.sh
@@ -6,8 +6,8 @@ for i in $(curl --key /etc/docker/ssl/calculonc-key.pem --cert /etc/docker/ssl/c
 do
 	echo $i 
         mysql -h $i -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL ON *.* TO '$MYSQL_PROXY_USER'@'$ipaddr' IDENTIFIED BY '$MYSQL_PROXY_PASSWORD'"
-        mysql -h 127.0.0.1 -P6032 -uadmin -padmin -e "INSERT INTO mysql_servers (hostgroup_id, hostname, port, max_replication_lag) VALUES (0, '$i', 3306, 20);"
+        mysql -h 127.0.0.1 -P6032 -uadmin -p$PROXYSQL_ADMIN_PASSWORD -e "INSERT INTO mysql_servers (hostgroup_id, hostname, port, max_replication_lag) VALUES (0, '$i', 3306, 20);"
  done
 
-mysql -h 127.0.0.1 -P6032 -uadmin -padmin -e "INSERT INTO mysql_users (username, password, active, default_hostgroup, max_connections) VALUES ('$MYSQL_PROXY_USER', '$MYSQL_PROXY_PASSWORD', 1, 0, 200);"
-mysql -h 127.0.0.1 -P6032 -uadmin -padmin -e "LOAD MYSQL SERVERS TO RUNTIME; SAVE MYSQL SERVERS TO DISK; LOAD MYSQL USERS TO RUNTIME; SAVE MYSQL USERS TO DISK;"
+mysql -h 127.0.0.1 -P6032 -uadmin -p$PROXYSQL_ADMIN_PASSWORD -e "INSERT INTO mysql_users (username, password, active, default_hostgroup, max_connections) VALUES ('$MYSQL_PROXY_USER', '$MYSQL_PROXY_PASSWORD', 1, 0, 200);"
+mysql -h 127.0.0.1 -P6032 -uadmin -p$PROXYSQL_ADMIN_PASSWORD -e "LOAD MYSQL SERVERS TO RUNTIME; SAVE MYSQL SERVERS TO DISK; LOAD MYSQL USERS TO RUNTIME; SAVE MYSQL USERS TO DISK;"
