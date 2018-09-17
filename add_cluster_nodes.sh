a4883a9be28578c512e7e0de1985d04e5642a370
Thorsten Weber <thorsten.weber@ruhmesmeile.com>
2018-09-17 09:52:38 +0200

    Fix hostgroup_id to proper value

--- add_cluster_nodes.sh
+++ add_cluster_nodes.sh
@@ -6,7 +6,7 @@ for i in $(curl --key /etc/docker/ssl/calculonc-key.pem --cert /etc/docker/ssl/c
 do
 	echo $i 
         mysql -h $i -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL ON *.* TO '$MYSQL_PROXY_USER'@'$ipaddr' IDENTIFIED BY '$MYSQL_PROXY_PASSWORD'"
-        mysql -h 127.0.0.1 -P6032 -uadmin -p$PROXYSQL_ADMIN_PASSWORD -e "INSERT INTO mysql_servers (hostgroup_id, hostname, port, max_replication_lag) VALUES (0, '$i', 3306, 20);"
+        mysql -h 127.0.0.1 -P6032 -uadmin -p$PROXYSQL_ADMIN_PASSWORD -e "INSERT INTO mysql_servers (hostgroup_id, hostname, port, max_replication_lag) VALUES (10, '$i', 3306, 20);"
  done
 
 mysql -h 127.0.0.1 -P6032 -uadmin -p$PROXYSQL_ADMIN_PASSWORD -e "INSERT INTO mysql_users (username, password, active, default_hostgroup, max_connections) VALUES ('$MYSQL_PROXY_USER', '$MYSQL_PROXY_PASSWORD', 1, 0, 200);"
